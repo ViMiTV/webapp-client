367955427b5a53f88c2940b3e47d7e9497dde96b
mddengo <mddengo@gmail.com>
2020-05-03 13:56:40 -0400

    entryway skeleton

---
+++ src/constants.js
@@ -0,0 +1,16 @@
+export const WIDTHS = {
+    WIDTH_1: 1,
+    WIDTH_2: 2,
+    WIDTH_3: 3,
+    WIDTH_4: 4,
+    WIDTH_5: 5,
+    WIDTH_6: 6,
+    WIDTH_7: 7,
+    WIDTH_8: 8,
+    WIDTH_9: 9,
+    WIDTH_10: 10,
+    WIDTH_11: 11,
+    WIDTH_12: 12
+};
+
+export const TEST = 1;
