f291f3cc629e55cfa73475a3bd067cad2bfac47a
mddengo <mddengo@gmail.com>
2020-05-03 11:52:13 -0400

    delete posenet code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
-import PoseNet from "./components/PoseNet"
 
 function App() {
   return (
-    <div className="App">
-      <PoseNet />
-    </div>
+    <div className="App"></div>
   );
 }
 
