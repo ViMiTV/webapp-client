1e70ed6cc02bf9782a15ad068fbbe190e18a7db4
mddengo <mddengo@gmail.com>
2020-05-16 15:20:05 -0400

    room code initial commit with helper files

--- functions/index.js
+++ functions/index.js
@@ -1,30 +1,23 @@
+// import {createPlayer} from './helpers/player';
+// import {ROOMS_COLLECTION, createRoom} from '.helpers/room';
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const {createRoom, MAX_CAPACITY, ROOMS_COLLECTION} = require('./helpers/room');
+const {createPlayer} = require('./helpers/player');
 
 admin.initializeApp({
     credential: admin.credential.applicationDefault(),
     databaseURL: 'https://vimi-tv.firebaseio.com',
 });
 
-const db = admin.firestore();
-
-const PLAYERS_COLLECTION = 'players';
-const ROOMS_COLLECTION = 'rooms';
-const MAX_CAPACITY = 6;
-
-exports.helloBobo = functions.https.onRequest((request, response) =>
-    response.send('hello bobo :)')
-);
-
-const createPlayer = (username) =>
-    db.collection(PLAYERS_COLLECTION).add({ username });
+// const db = admin.firestore();
 
 const addPlayerToRoom = (player, roomId) => {
     // TODO: check if room is full?
     createPlayer(player)
         .then((ref) => {
             const playerId = ref.id;
-            return db
+            return admin.firestore()
                 .collection(ROOMS_COLLECTION)
                 .doc(roomId)
                 .update({
@@ -35,16 +28,9 @@ const addPlayerToRoom = (player, roomId) => {
         .catch();
 };
 
-const createRoom = (maxCapacity, initialPlayerId) =>
-    db.collection(ROOMS_COLLECTION).add({
-        maxCapacity,
-        players: admin.firestore.FieldValue.arrayUnion(initialPlayerId),
-    });
-
 exports.createRoom = functions.https.onRequest((request, response) => {
     try {
         const username = request.query.username;
-        console.log(request.query.username);
         if (!username) {
             response.send('Missing username!');
         }
@@ -52,7 +38,7 @@ exports.createRoom = functions.https.onRequest((request, response) => {
         return createPlayer(username)
             .then((playerRef) => createRoom(MAX_CAPACITY, playerRef.id))
             .then((roomRef) => response.send(`created room ${roomRef.id}`))
-            .catch((e) => console.log(`create player and room ${e}`));
+            .catch((e) => console.log(`create player and room error: ${e}`));
     } catch (e) {
         console.log(`create room error: ${e}`);
     }
