851c0d8f46d9266b3c459425060f7549ca23b4a8
mddengo <mddengo@gmail.com>
2020-05-16 21:33:06 -0400

    Md joinroom (#4)
    * buttons disabled if username is empty
    * user can create room from client
    * update api to join room

--- functions/index.js
+++ functions/index.js
@@ -12,48 +12,124 @@ const PLAYERS_COLLECTION = 'players';
 const ROOMS_COLLECTION = 'rooms';
 const MAX_CAPACITY = 6;
 
-exports.helloBobo = functions.https.onRequest((request, response) =>
-    response.send('hello bobo :)')
-);
-
-const createPlayer = (username) =>
-    db.collection(PLAYERS_COLLECTION).add({ username });
-
-const addPlayerToRoom = (player, roomId) => {
-    // TODO: check if room is full?
-    createPlayer(player)
-        .then((ref) => {
-            const playerId = ref.id;
-            return db
-                .collection(ROOMS_COLLECTION)
-                .doc(roomId)
-                .update({
-                    players: admin.firestore.FieldValue.arrayUnion(playerId),
-                });
-        })
-        .then((ref) => console.log(`added ${player} to room ${roomId}`))
-        .catch();
-};
+// from http://guid.us/GUID/JavaScript
+const generateRoomCode = () => (((1+Math.random())*0x10000)|0).toString(16).substring(1).toUpperCase();
 
 const createRoom = (maxCapacity, initialPlayerId) =>
     db.collection(ROOMS_COLLECTION).add({
         maxCapacity,
         players: admin.firestore.FieldValue.arrayUnion(initialPlayerId),
+        roomCode: generateRoomCode()
     });
+
+const createPlayer = username =>
+    db.collection(PLAYERS_COLLECTION).add({
+        username,
+        timestamp: new Date().toTimeString()
+    });
+
+const getRoomIdByCode = async roomCode => {
+    const snapshot = await db
+        .collection(ROOMS_COLLECTION)
+        .where('roomCode', '==', roomCode)
+        .get();
+
+    if (snapshot.empty) {
+        console.log('Code is invalid or room does not exist');
+    } else {
+        return snapshot.docs[0].id;
+    }
+};
+
+const addPlayerToRoom = (player, roomId) => {
+    // TODO: check if room is full (for player-limited games)
+    return createPlayer(player).then((ref) => {
+        const playerId = ref.id;
+        return db
+            .collection(ROOMS_COLLECTION)
+            .doc(roomId)
+            .update({
+                players: admin.firestore.FieldValue.arrayUnion(playerId),
+            });
+    });
+};
+
+const getRoomCodeById = roomId =>
+    db.collection(ROOMS_COLLECTION)
+    .doc(roomId)
+    .get();
 
 exports.createRoom = functions.https.onRequest((request, response) => {
     try {
+        response.set('Access-Control-Allow-Origin', '*');
+
         const username = request.query.username;
-        console.log(request.query.username);
         if (!username) {
             response.send('Missing username!');
+        } else {
+            return createPlayer(username)
+                .then((playerRef) => createRoom(MAX_CAPACITY, playerRef.id))
+                .then((roomRef) => response.send({roomId: roomRef.id}))
+                .catch((e) =>
+                    console.log(`create player and room error: ${e}`)
+                );
         }
-        console.log(username);
-        return createPlayer(username)
-            .then((playerRef) => createRoom(MAX_CAPACITY, playerRef.id))
-            .then((roomRef) => response.send(`created room ${roomRef.id}`))
-            .catch((e) => console.log(`create player and room ${e}`));
     } catch (e) {
         console.log(`create room error: ${e}`);
+    }
+});
+
+exports.getRoomCode = functions.https.onRequest((request, response) => {
+    try {
+        response.set('Access-Control-Allow-Origin', '*');
+
+        const roomId = request.query.roomId;
+        if (!roomId) {
+            response.send('Missing room id');
+        } else {
+            return getRoomCodeById(roomId)
+                .then(doc => {
+                    if (!doc.exists) {
+                        console.log(`room ${roomId} does not exist`);
+                    } else {
+                        console.log(`entrycode: ${doc.data().roomCode}`);
+                        response.send({roomCode: doc.data().roomCode});
+                    }
+                    return;
+                })
+                .catch((e) => console.log(`Get room code by id error: ${e}`));
+        }
+    } catch (e) {
+        response.send(`getRoomCode error: ${e}`);
+    }
+})
+
+exports.joinRoom = functions.https.onRequest((request, response) => {
+    try {
+        response.set('Access-Control-Allow-Origin', '*');
+
+        const roomCode = request.query.roomCode;
+        const username = request.query.username;
+        console.log(roomCode);
+        console.log(username);
+
+        if (!roomCode || !username) {
+            response.send('Missing room code or username');
+        } else {
+            return getRoomIdByCode(roomCode)
+                .then((roomId) => addPlayerToRoom(username, roomId))
+                .then(() =>
+                    response.send(
+                        `added user ${username} with room code ${roomCode}`
+                    )
+                )
+                .catch((e) =>
+                    response.send(
+                        `add player ${username} to room ${roomId} error: ${e}`
+                    )
+                );
+        }
+    } catch (e) {
+        response.send(`unexpected join room error: ${e}`);
     }
 });
--- src/components/entryWay.js
+++ src/components/entryWay.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -8,51 +8,92 @@ import FormControl from 'react-bootstrap/FormControl';
 import './entryWay.css';
 import { WIDTHS, BASE_URL } from '../constants';
 
-const handleCreateRoomOnClick = (username) => {
-    console.log(`create room for ${username}`);
-    fetch(`${BASE_URL}createRoom?username=${username}`).then(
-        (res) => console.log(res),
-        (error) => console.log(error)
-    );
-};
-const handleJoinRoomOnClick = (username) =>
-    console.log(`join room as ${username}`);
+class Entry extends Component {
+    state = {
+        roomCode: '',
+        username: '',
+        disableButtons: true,
+    };
 
-export const EntryWay = () => {
-    const [username, setUsername] = useState('');
+    handleCreateRoomOnClick = (username) =>
+        fetch(`${BASE_URL}createRoom?username=${username}`).then((res) =>
+            res
+                .json()
+                .then((res) => this.getRoomCode(res.roomId))
+                .catch((error) => console.log(error))
+        );
 
-    return (
-        <div className='EntryWay'>
-            <Container fluid>
-                <Row xs={WIDTHS.WIDTH_12}>
-                    <Col xs={WIDTHS.WIDTH_4}>
-                        <InputGroup>
-                            <FormControl
-                                placeholder='Enter your name'
-                                aria-label='Enter Your Name'
-                                aria-describedby='basic-addon2'
-                                onChange={(e) => setUsername(e.target.value)}
-                            />
-                        </InputGroup>
-                    </Col>
-                </Row>
-                <Row xs={WIDTHS.WIDTH_6}>
-                    <Col md={{ offset: WIDTHS.WIDTH_1 }}>
-                        <Button
-                            variant='outline-info'
-                            onClick={() => handleCreateRoomOnClick(username)}
-                        >
-                            Create Room
-                        </Button>{' '}
-                        <Button
-                            variant='outline-info'
-                            onClick={() => handleJoinRoomOnClick(username)}
-                        >
-                            Join Room
-                        </Button>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    );
-};
+    getRoomCode = (roomId) =>
+        fetch(`${BASE_URL}getRoomCode?roomId=${roomId}`)
+            .then((res) => res.json())
+            .then((res) => this.setState({ roomCode: res.roomCode }))
+            .catch((error) => console.log(error));
+
+    handleJoinRoomOnClick = (username) => {
+        console.log(`join room as ${username}`);
+    };
+
+    handleUsernameOnChange = (e) => {
+        const username = e.target.value;
+        this.setState({ username: e.target.value, disableButtons: false });
+
+        if (!username) {
+            this.setState({ disableButtons: true });
+        } else {
+            this.setState({ disableButtons: false });
+        }
+    };
+
+    render() {
+        return (
+            <div className='EntryWay'>
+                <Container fluid>
+                    <Row xs={WIDTHS.WIDTH_12}>
+                        <Col xs={WIDTHS.WIDTH_4}>
+                            <InputGroup>
+                                <FormControl
+                                    required
+                                    type='text'
+                                    placeholder='Enter your name'
+                                    aria-label='Enter Your Name'
+                                    aria-describedby='basic-addon2'
+                                    onChange={this.handleUsernameOnChange}
+                                />
+                            </InputGroup>
+                        </Col>
+                    </Row>
+                    <Row xs={WIDTHS.WIDTH_6}>
+                        {this.state.roomCode}
+                        <Col md={{ offset: WIDTHS.WIDTH_1 }}>
+                            <Button
+                                variant='outline-info'
+                                disabled={this.state.disableButtons}
+                                onClick={() =>
+                                    this.handleCreateRoomOnClick(
+                                        this.state.username
+                                    )
+                                }
+                            >
+                                Create Room
+                            </Button>{' '}
+                            <Button
+                                variant='outline-info'
+                                disabled={this.state.disableButtons}
+                                onClick={(e) =>
+                                    this.handleJoinRoomOnClick(
+                                        e,
+                                        this.state.username
+                                    )
+                                }
+                            >
+                                Join Room
+                            </Button>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        );
+    }
+}
+
+export const EntryWay = Entry;
